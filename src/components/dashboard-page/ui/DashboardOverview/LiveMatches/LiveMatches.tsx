cd906eadf4c251f796a92b121ad3ab147a82dcbf
NguyenRobin <robin.nnguyen@hotmail.com>
2024-12-09 20:23:57 +0100

    fix

--- src/components/dashboard-page/ui/DashboardOverview/LiveMatches/LiveMatches.tsx
+++ src/components/dashboard-page/ui/DashboardOverview/LiveMatches/LiveMatches.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import CardWrapper from '../../../../ui/card-wrapper/CardWrapper';
 import { RiLiveLine } from 'react-icons/ri';
 import './LiveMatches.scss';
 import { getMatchesByStatus } from '../../../../../features/matches/server/actions/match';
 import ListTournamentMatches from '../../../../../features/matches/components/ui/ListTournamentMatches/ListTournamentMatches';
 
-async function LiveMatches() {
+export default async function LiveMatches() {
   let response = await getMatchesByStatus('ongoing', 3);
 
   if (response.status !== 200) {
@@ -19,25 +19,25 @@ async function LiveMatches() {
   }
 
   return (
-    <CardWrapper>
-      <section className="live-matches__title">
-        <RiLiveLine />
-        <h2>Pågående matcher</h2>
-      </section>
+    <Suspense fallback={<p>laddar</p>}>
+      <CardWrapper>
+        <section className="live-matches__title">
+          <RiLiveLine />
+          <h2>Pågående matcher</h2>
+        </section>
 
-      <section className="live-matches__ongoing">
-        {!matches.length ? (
-          <p>Inga pågående live matcher.</p>
-        ) : (
-          <ListTournamentMatches matches={matches} />
-        )}
-      </section>
+        <section className="live-matches__ongoing">
+          {!matches.length ? (
+            <p>Inga pågående live matcher.</p>
+          ) : (
+            <ListTournamentMatches matches={matches} />
+          )}
+        </section>
 
-      <section className="">
-        {/* <ListTournamentMatches matches={matches} /> */}
-      </section>
-    </CardWrapper>
+        <section className="">
+          {/* <ListTournamentMatches matches={matches} /> */}
+        </section>
+      </CardWrapper>
+    </Suspense>
   );
 }
-
-export default LiveMatches;
